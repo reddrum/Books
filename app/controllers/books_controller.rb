d8effe6ae44e75f4dd622db67fc4d8993ada50cc
“reddrum” <anton.kranz@me.com>
2017-01-30 20:27:39 +0300

    Add some views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,12 @@
 class BooksController < ApplicationController
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def index
+    @books = Book.all.order("created_at DESC")
+  end
 
+  def show
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -10,12 +15,23 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+
   end
 
   private
 
   def book_params
     params.require(:book).permit(:title, :description, :author)
+  end
+
+  def find_book
+    @book = Book.find(params[:id])
   end
 
 end
