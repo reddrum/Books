0e7e489edf448427be31000a8a6d0f344d23e946
“reddrum” <anton.kranz@me.com>
2017-11-26 22:22:41 +0300

    setup travis

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.secret_key = 'DEVISE_KEY' if Rails.env.production?
+  config.secret_key = ENV['SECRET_KEY']
 
   config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', info_fields: 'email,name'
 
